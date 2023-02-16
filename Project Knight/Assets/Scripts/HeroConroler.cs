c176a08ae5cc94833c242cd0a1def8af7f249154
Makseke <makseke@mail.ru>
2023-02-16 17:32:39 +0300

    Better animetion

--- Project Knight/Assets/Scripts/HeroConroler.cs
+++ Project Knight/Assets/Scripts/HeroConroler.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using TreeEditor;
+//using TreeEditor;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -11,25 +11,26 @@ using UnityEngine;
     public Vector2 touchEndPosition;
     public Vector2 touchfinalPosition;
 
-    public float speed = 0.2f;
+    private float speed;
+    public AnimationCurve movemetCurve;
 
     public bool isMoving = false;
-    public bool inProcess = false;
-    public int moveDirection = 0;
 
     public Vector2 startPosition;
     public Vector2 endPosition;
+    public float time;
 
     private void Move()
     {
         if (endPosition != hero.position)
         {
             isMoving = true;
-            inProcess = true;
         }
         if (isMoving == true)
         {
-                hero.transform.position = Vector2.MoveTowards(hero.position, endPosition, speed * Time.deltaTime);
+            speed = movemetCurve.Evaluate(time);
+            hero.transform.position = Vector2.MoveTowards(hero.position, endPosition, speed * Time.deltaTime);
+            time += Time.deltaTime;
         }
         else
         {
@@ -38,16 +39,16 @@ using UnityEngine;
         if (hero.position == endPosition)
         {
             isMoving = false;
-            inProcess = false;
+            time = 0.0f;
         }
     }
 
     void Start()
     {
         hero = GetComponent<Rigidbody2D>();
+        time = Time.deltaTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.touchCount == 1) {
@@ -83,53 +84,48 @@ using UnityEngine;
                     y_dif = Mathf.Abs(touchEndPosition.y) - Mathf.Abs(touchStartPosition.y);
                 }
 
-                if (x_dif > y_dif)
+                if (x_dif > 100 || y_dif > 100)
                 {
-                    if (touchStartPosition.x > touchEndPosition.x)
+                    if (x_dif > y_dif)
                     {
-                        //hero.MovePosition(hero.position - new Vector2(1.0f, 0.0f));
-                        //hero.transform.position = Vector2.Lerp(hero.position, new Vector2(hero.position.x - 1, 0), speed * Time.deltaTime);
-                        //hero.transform.position = Vector2.MoveTowards(hero.position, new Vector2(hero.position.x - 1, 0), Time.deltaTime * speed);
-
-                        if (inProcess == false)
+                        if (touchStartPosition.x > touchEndPosition.x)
                         {
-                            endPosition = hero.position - new Vector2(1, 0);
+                            if (isMoving == false)
+                            {
+                                endPosition = hero.position - new Vector2(1, 0);
+                            }
+                        }
+                        else
+                        {
+                            if (isMoving == false)
+                            {
+                                endPosition = hero.position + new Vector2(1, 0);
+                            }
                         }
                     }
                     else
                     {
-                        //hero.MovePosition(hero.position + new Vector2(1.0f, 0.0f));
-                        if (inProcess == false)
+                        if (touchStartPosition.y > touchEndPosition.y)
                         {
-                            endPosition = hero.position + new Vector2(1, 0);
+                            if (isMoving == false)
+                            {
+                                endPosition = hero.position - new Vector2(0, 1);
+                            }
+                        }
+                        else
+                        {
+                            if (isMoving == false)
+                            {
+                                endPosition = hero.position + new Vector2(0, 1);
+                            }
                         }
                     }
                 }
-                else
-                {
-                    if (touchStartPosition.y > touchEndPosition.y)
-                    {
-                        //hero.MovePosition(hero.position - new Vector2(0.0f, 1.0f));
-                        if (inProcess == false)
-                        {
-                            endPosition = hero.position - new Vector2(0, 1);
-                        }
-                    }
-                    else
-                    {
-                        //hero.MovePosition(hero.position + new Vector2(0.0f, 1.0f));
-                        if (inProcess == false)
-                        {
-                            endPosition = hero.position + new Vector2(0, 1);
-                        }
-                    }
-                }
-
             }
         }
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         Move();
     }
