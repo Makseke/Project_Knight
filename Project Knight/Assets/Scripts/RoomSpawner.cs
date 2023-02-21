e707c3398210814f2c6e9ee9e9f3af2b56d334e0
Makseke <makseke@mail.ru>
2023-02-21 16:03:24 +0300

    Map Generation

---
+++ Project Knight/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    private RoomsType variants;
+    private int rand;
+    private bool spawned = false;
+    private float waitTime = 3f;
+
+    void Start()
+    {
+        variants = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomsType>();
+        Destroy(gameObject, waitTime);
+        Invoke("Spawn", 0.2f);
+    }
+
+    public void Spawn()
+    {
+        if (!spawned)
+        {
+            rand = Random.RandomRange(0, variants.basicRooms.Length);
+            Instantiate(variants.basicRooms[rand], transform.position, variants.basicRooms[rand].transform.rotation);
+            spawned = true;
+        }
+    }
+
+    private void DoubleSpawn(Collider2D other)
+    {
+        if(other.CompareTag("RoomPoint") && other.GetComponent<RoomSpawner>().spawned)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
